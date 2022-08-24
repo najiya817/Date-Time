579b9dcce13381bc5dfc4b31818915dbd41995a8
NAJIYA NASRIN <110896779+najiya817@users.noreply.github.com>
2022-08-24 13:57:58 +0530

    message

---
+++ DateTimePrograms.py
@@ -0,0 +1,9 @@
+import datetime
+print("current date and time: ",datetime.datetime.now())
+print("current year: ",datetime.date.today().strftime("%Y"))
+print("Month  of the year : ",datetime.date.today().strftime("%B"))
+print("Week number of the year: ",datetime.date.today().strftime("%W"))
+print("weekday of the week: ",datetime.date.today().strftime("%w"))
+print("Day of the year: ",datetime.date.today().strftime("%j"))
+print("Day of the month: ",datetime.date.today().strftime("%d"))
+print("Day of the week: ",datetime.date.today().strftime("%A"))
